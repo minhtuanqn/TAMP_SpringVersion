d9a80f7e83d90fe1893dd5625dc5aeebbb787725
minhtuanqn <minhtuanqn320@gmail.com>
2022-09-16 16:15:31 +0700

    finish-delete-supplier

--- src/main/java/com/tamp_backend/controller/SupplierController.java
+++ src/main/java/com/tamp_backend/controller/SupplierController.java
@@ -1,6 +1,5 @@
 package com.tamp_backend.controller;
 
-import com.tamp_backend.model.CategoryModel;
 import com.tamp_backend.model.ResponseModel;
 import com.tamp_backend.model.supplier.CreateSupplierModel;
 import com.tamp_backend.model.supplier.SupplierModel;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.multipart.MultipartFile;
 import javax.validation.Valid;
+import java.util.UUID;
 
 @RestController
 @Validated
@@ -24,12 +24,32 @@ public class SupplierController {
         this.supplierService = supplierService;
     }
 
+    /**
+     * Create new supplier
+     * @param requestModel
+     * @param logo
+     * @return response model contains created supplier model
+     */
     @PostMapping(path = "", produces = {MediaType.APPLICATION_JSON_VALUE}, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<ResponseModel> createCategory(@Valid @ModelAttribute CreateSupplierModel requestModel, @RequestPart MultipartFile logo) {
+    public ResponseEntity<ResponseModel> createSupplier(@Valid @ModelAttribute CreateSupplierModel requestModel, @RequestPart MultipartFile logo) {
         SupplierModel savedSupplierModel = supplierService.createSupplier(requestModel, "");
         ResponseModel responseModel = new ResponseModel().statusCode(HttpStatus.OK.value())
                 .data(savedSupplierModel)
                 .message("Created successfully");
+        return new ResponseEntity<>(responseModel, HttpStatus.OK);
+    }
+
+    /**
+     * Delete a supplier model
+     * @param id
+     * @return response model contains deleted supplier model
+     */
+    @DeleteMapping(path = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<ResponseModel> deleteSupplier(@PathVariable UUID id) {
+        SupplierModel deletedSupplierModel = supplierService.deleteSupplier(id);
+        ResponseModel responseModel = new ResponseModel().statusCode(HttpStatus.OK.value())
+                .data(deletedSupplierModel)
+                .message("OK");
         return new ResponseEntity<>(responseModel, HttpStatus.OK);
     }
 }
--- src/main/java/com/tamp_backend/service/SupplierService.java
+++ src/main/java/com/tamp_backend/service/SupplierService.java
@@ -3,6 +3,7 @@ package com.tamp_backend.service;
 import com.tamp_backend.constant.EntityStatusEnum;
 import com.tamp_backend.constant.UserEnum;
 import com.tamp_backend.customexception.DuplicatedEntityException;
+import com.tamp_backend.customexception.NoSuchEntityException;
 import com.tamp_backend.entity.AccountEntity;
 import com.tamp_backend.entity.SupplierEntity;
 import com.tamp_backend.entity.WalletEntity;
@@ -51,12 +52,14 @@ public class SupplierService {
         if (accountRepository.existsByEmail(createSupplierModel.getEmail()))
             throw new DuplicatedEntityException("This email has been already for another account");
 
+        //Save new account
         AccountEntity createdAccount = modelMapper.map(createSupplierModel, AccountEntity.class);
         createdAccount.setStatus(EntityStatusEnum.AccountStatusEnum.ACTIVE.ordinal());
         createdAccount.setRole(UserEnum.RoleEnum.SUPPLIER.toString());
         createdAccount.setPassword(passwordEncoder.encode(createSupplierModel.getPassword()));
         AccountEntity insertedAccount = accountRepository.save(createdAccount);
 
+        //Save new information of supplier
         SupplierEntity createdSupplier = modelMapper.map(createSupplierModel, SupplierEntity.class);
         createdSupplier.setStatus(EntityStatusEnum.AccountStatusEnum.ACTIVE.ordinal());
         createdSupplier.setAccountId(insertedAccount.getId());
@@ -66,6 +69,7 @@ public class SupplierService {
         createdSupplier.setCreateBy(UUID.fromString("e5facdba-b1b5-4077-8483-a9bfbb187955"));
         SupplierEntity insertedSupplier = supplierRepository.save(createdSupplier);
 
+        //Create new wallet for supplier
         WalletEntity createdWallet = new WalletEntity();
         createdWallet.setAccountId(insertedAccount.getId());
         createdWallet.setOwnerType(UserEnum.WalletTypeEnum.SUPPLIER.toString());
@@ -74,8 +78,36 @@ public class SupplierService {
         createdWallet.setCreateAt(LocalDateTime.now());
         WalletEntity insertedWallet = walletRepository.save(createdWallet);
 
+        //Return saved supplier model
         SupplierModel supplierModel = modelMapper.map(insertedSupplier, SupplierModel.class);
         supplierModel.setAccountModel(modelMapper.map(insertedAccount, AccountModel.class));
+        return supplierModel;
+    }
+
+    /**
+     * Delete a supplier by id
+     * @param supplierId
+     * @return deleted supplier model
+     */
+    public SupplierModel deleteSupplier(UUID supplierId) {
+        Optional<SupplierEntity> optionalSupplierEntity = supplierRepository.findById(supplierId);
+        SupplierEntity supplierEntity = optionalSupplierEntity.orElseThrow(() -> new NoSuchEntityException("Not found supplier with id"));
+
+        Optional<AccountEntity> optionalAccountEntity = accountRepository.findById(supplierEntity.getAccountId());
+        AccountEntity accountEntity = optionalAccountEntity.orElseThrow(() -> new NoSuchEntityException("Not found supplier with id"));
+
+        //Update supplier information
+        supplierEntity.setStatus(EntityStatusEnum.AccountStatusEnum.DISABLE.ordinal());
+        supplierEntity.setUpdateAt(LocalDateTime.now());
+        SupplierEntity deletedSupplierEntity = supplierRepository.save(supplierEntity);
+
+        //Update supplier account
+        accountEntity.setStatus(EntityStatusEnum.AccountStatusEnum.DISABLE.ordinal());
+        AccountEntity deletedAccountEntity = accountRepository.save(accountEntity);
+
+        //Return deleted supplier model
+        SupplierModel supplierModel = modelMapper.map(deletedSupplierEntity, SupplierModel.class);
+        supplierModel.setAccountModel(modelMapper.map(deletedAccountEntity, AccountModel.class));
         return supplierModel;
     }
 }
